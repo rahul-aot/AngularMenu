dd5ef4a971ade3241d57ebc0c25852df46563d95
rahulap-aot <rahul.ap@aot-technologies.com>
2024-12-05 02:58:31 -0800

    first commit

---
+++ src/app/pages/pages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pages',
+  imports: [],
+  standalone: true,
+  templateUrl: './pages.component.html',
+  styleUrl: './pages.component.scss'
+})
+export class PagesComponent {
+
+}
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  imports: [],
+  standalone: true,
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.scss'
+})
+export class ProductsComponent {
+
+}
