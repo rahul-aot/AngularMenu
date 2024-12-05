116eee2c3ac9a500aa982e3f2f4e628a84012323
rahulap-aot <rahul.ap@aot-technologies.com>
2024-12-05 03:25:27 -0800

    leftside

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LeftSideBarComponent } from "./left-side-bar/left-side-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LeftSideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/left-side-bar/left-side-bar.component.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-left-side-bar',
+  imports: [RouterModule,CommonModule],
+  templateUrl: './left-side-bar.component.html',
+  styleUrl: './left-side-bar.component.scss'
+})
+export class LeftSideBarComponent {
+  isLeftSidebarCollapsed = input.required<boolean>();
+  changeIsLeftSidebarCollapsed = output<boolean>();
+  items = [
+    {
+      routeLink: 'dashboard',
+      icon: 'fal fa-home',
+      label: 'Dashboard',
+    },
+    {
+      routeLink: 'products',
+      icon: 'fal fa-box-open',
+      label: 'Products',
+    },
+    {
+      routeLink: 'pages',
+      icon: 'fal fa-file',
+      label: 'Pages',
+    },
+    {
+      routeLink: 'settings',
+      icon: 'fal fa-cog',
+      label: 'Settings',
+    },
+  ];
+
+  toggleCollapse(): void {
+    this.changeIsLeftSidebarCollapsed.emit(!this.isLeftSidebarCollapsed());
+  }
+
+  closeSidenav(): void {
+    this.changeIsLeftSidebarCollapsed.emit(true);
+  }
+}
